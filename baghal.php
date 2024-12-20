fcd692604addd2e51073535277ba83c0376a5f5b
maa44 <ademahdi88@gmail.com>
2024-12-20 22:16:36 +0330

    Add files via upload

---
+++ baghal.php
@@ -0,0 +1,14 @@
+<?php
+include("header.html");
+?>
+
+<section class="container text-center">
+
+ <div class="row row-cols-3"> >
+ <div class="col"><img class="img-fluid rounded-5" src="img/front-view-chest.jpg"><a href="lat.php">لت از جلو</a></div>
+ </div>
+</section>
+  
+<?php
+include("footer.html");
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include("header.html");
+?>
+
+<section class="container text-center">
+
+  <div class="row row-cols-3">
+    <div class="col"><img class="img-fluid rounded-5" src="img/front-view-chest.jpg"><a href="">سینه</a></div>
+    <div class="col"><img class="img-fluid rounded-5" src="img/back-view-zirbaghal.jpg"><a href="baghal.php">پشت و زیر بغل </a></div>
+    <div class="col"><img class="img-fluid rounded-5" src="img/front-view-arms.jpg">جلو بازو</div>
+    <div class="col"><img class="img-fluid rounded-5" src="img/front-view-leg-black-1.jpg">پا</div>  
+  </div>
+</div>
+
+        </section>
+<?php
+include("footer.html");
+?>
+  
