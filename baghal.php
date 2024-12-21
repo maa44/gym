ca5232f6d8c542400fa696dcf773e5fb5014ca96
maa44 <ademahdi88@gmail.com>
2024-12-21 17:04:11 +0330

    Add files via upload

--- baghal.php
+++ baghal.php
@@ -5,7 +5,7 @@ include("header.html");
 <section class="container text-center">
 
  <div class="row row-cols-3"> >
- <div class="col"><img class="img-fluid rounded-5" src="img/front-view-chest.jpg"><a href="lat.php">لت از جلو</a></div>
+ <div class="col text-dark"><img class="img-fluid rounded-5" src="img/front-view-chest.jpg"><a class="text-dark" href="lat.php">لت از جلو</a></div>
  </div>
 </section>
   
