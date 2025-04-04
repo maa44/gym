dfe290391e94c037ed9e6ebc6c5cb2b2dc5ed656
maa44 <ademahdi88@gmail.com>
2025-04-04 21:18:59 +0330

    Add files via upload

--- detail.php
+++ detail.php
@@ -1,20 +1,23 @@
-
 <?php
 include("header.php");
-
+$id=0;
+$id=$_GET["id"];
+if (isset($_GET['id']))
+	 $id=$_GET['id'];
 
 $link=mysqli_connect("localhost","root","","gym");
-$result=mysqli_query($link,"SELECT * FROM `new`");
+$result=mysqli_query($link,"SELECT * FROM `new` WHERE id='$id'");
 mysqli_close($link);
 
 $row=mysqli_fetch_array($result);
 while($row){
-?>
+?> 
 
 <div class="row card m-1">
     <img class="col-12 col-md-6 card-image mt-1" src="<?php echo($row["imageurl"]); ?>" >
     <span class="col-12 m-2 h5"><?php echo($row["title"]);?></span>
-    <b><a href="detail.php?id=<?php echo ($row['id']) ?>" style="text-decoration: none;">توضيحات تكميلي</a></b></div>
+    <span class="col-12 m-2"><?php echo($row["text"]); ?></span>
+</div>
 
 <?php
 $row=mysqli_fetch_array($result);
@@ -23,4 +26,3 @@ $row=mysqli_fetch_array($result);
 <?php
 include("footer.html");
 ?>
-  
